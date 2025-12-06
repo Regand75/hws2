5044ac504f776aeb501f6d04e8dfa99c0ecc144c
Уразов Андрей <regandess@gmail.com>
2025-12-06 19:41:28 +0300

    connection component HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        const restored = restoreState<string>('hw6-editable-span-value', '');
+        setValue(restored);
     }
 
     return (
